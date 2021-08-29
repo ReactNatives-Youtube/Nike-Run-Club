554e1884ed79677d073439ee44f41968e88320dd
nakulBageja <nbageja@gmail.com>
2021-08-29 15:38:17 +0530

    Completion of text input

--- constants/Validation.js
+++ constants/Validation.js
@@ -1,6 +1,13 @@
-  // Helper function for validation of metric values
-  export const validateInput = (input)=>{
-    // The distance should have any number of digits from 0 to 9. One or zero decimal point. One or zero number (digit) after the decimal point.
-    var rgx = /^[0-9]{1,}\.?[0-9]?$/;
-    return input.match(rgx); // true or false
+// Helper function for validation of metric values
+export const validateInput = (input, typeOfMetric) => {
+  var rgx;
+  // The distance should have any number of digits from 0 to 9. One or zero decimal point. One or zero number (digit) after the decimal point.
+  if (typeOfMetric == 'Distance') {
+    console.log(typeOfMetric);
+    rgx = /^[0-9]{0,2}\.{1}[0-9]{0,2}?$/;
+  } else {
+    // The time - hours and minutes. The hours and minutes can be >=0 and <=99
+    rgx = /^[0-9]{0,2}\:{1}[0-9]{0,2}?$/;
   }
+  return input.match(rgx); // true or false
+};
--- src/screens/Home/Run/index.js
+++ src/screens/Home/Run/index.js
@@ -5,40 +5,42 @@
 import React, {useState} from 'react';
 import {View, Text, Pressable, TextInput} from 'react-native';
 import {Avatar} from 'react-native-elements';
-import { validateInput } from '../../../../constants/Validation';
+import {validateInput} from '../../../../constants/Validation';
 import styles from './styles';
 const RunScreen = () => {
-  
-  // States: 
-    // 1. For metric value
-  const [metricValue, setMetricValue] = useState('0.1'); 
-    // 2. Toggling
-  const [Toggle, setToggle] = useState('Distance')
-    // 3. Metric Unit
-  const [MetricUnit, setMetricUnit] = useState('Kilometers')
+  // States:
+  // 1. For metric value
+  const [metricValue, setMetricValue] = useState('1.0');
+  // 2. Toggling
+  const [Toggle, setToggle] = useState('Distance');
+  // 3. Metric Unit
+  const [MetricUnit, setMetricUnit] = useState('Kilometers');
 
   // Helper function to make changes to the text input
   const changeMetricValueHandler = input => {
-    // For distance
-    // To round off the number to 1 decimal   eg:   2.5 / 2.0 /1.8 etc
-    if (validateInput(input)) {
+    if (validateInput(input, Toggle)) {
+      if (input[0] == '.' || input[0] == ':') {
+        input = '0' + input;
+      }
+      if (input[input.length - 1] == '.' || input[input.length - 1] == ':') {
+        input = input + '0';
+      }
       setMetricValue(input);
     }
-    // For time
-   
   };
 
   // Toggle Function
-  const toggleHandler = ()=>{
-    if(Toggle == 'Distance')  {
-      setToggle('Time')
-      setMetricUnit('Minutes')
-    }else{
-      setToggle('Distance')
-      setMetricUnit('Kilometers')
+  const toggleHandler = () => {
+    if (Toggle == 'Distance') {
+      setToggle('Time');
+      setMetricUnit('Hours : Minutes');
+      setMetricValue('01:00');
+    } else {
+      setToggle('Distance');
+      setMetricUnit('Kilometers');
+      setMetricValue('1.0');
     }
-  }
-
+  };
 
   return (
     <View style={styles.mainContainer}>
@@ -65,9 +67,7 @@ const RunScreen = () => {
           containerStyle={styles.avatarContainer}
         />
         {/* Toggle button to change the metric from distance or time */}
-        <Pressable
-          onPress={toggleHandler}
-          style={styles.toggleContainer}>
+        <Pressable onPress={toggleHandler} style={styles.toggleContainer}>
           <Text style={styles.toggleTitle}>{Toggle}</Text>
         </Pressable>
       </View>
