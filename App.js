a713fc2f5aa827162efa7ffd033805916ca876bc
nakulBageja <nbageja@gmail.com>
2021-08-30 21:30:39 +0530

    Addition of google maps

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import HomeScreen from './src/screens/Home';
 
-
 const App: () => Node = () => {
-
-  return (
-   <HomeScreen/>
-  );
+  return <HomeScreen />;
 };
-
 
 export default App;
--- src/screens/Home/Run/index.js
+++ src/screens/Home/Run/index.js
@@ -4,6 +4,7 @@
 
 import React, {useState} from 'react';
 import {View, Text, Pressable, TextInput} from 'react-native';
+import MapView from 'react-native-maps';
 import {Avatar} from 'react-native-elements';
 import {validateInput} from '../../../../constants/Validation';
 import styles from './styles';
@@ -43,33 +44,57 @@ const RunScreen = () => {
   };
 
   return (
-    <View style={styles.mainContainer}>
+    <View style={{height: '100%', width: '100%'}}>
       {/* Google Maps API/Image */}
-      {/* Metric - Button to change the metric value*/}
-      <View>
-        <TextInput
-          style={styles.metricValue}
-          keyboardType="decimal-pad"
-          value={metricValue}
-          onChangeText={changeMetricValueHandler}
+      <View style={{height: '100%', width: '100%'}} pointerEvents="none">
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          minZoomLevel={18}
+          style={{flex: 1, opacity: 0.5}}
         />
-        <Text style={styles.metricUnit}>{MetricUnit}</Text>
       </View>
-      {/* Start Button */}
-      <View style={styles.bottomContainer}>
-        <Avatar
-          size={120}
-          rounded
-          title="START"
-          onPress={() => console.warn('Works!')}
-          activeOpacity={0.7}
-          titleStyle={styles.avatarTitle}
-          containerStyle={styles.avatarContainer}
-        />
-        {/* Toggle button to change the metric from distance or time */}
-        <Pressable onPress={toggleHandler} style={styles.toggleContainer}>
-          <Text style={styles.toggleTitle}>{Toggle}</Text>
-        </Pressable>
+
+      {/* <Text style={{position: 'absolute', bottom: 0, right: 0, fontSize: 53}}>
+        Hello
+      </Text> */}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          ...styles.mainContainer,
+        }}>
+        {/* Metric - Button to change the metric value*/}
+        <View>
+          <TextInput
+            style={styles.metricValue}
+            keyboardType="decimal-pad"
+            value={metricValue}
+            onChangeText={changeMetricValueHandler}
+          />
+          <Text style={styles.metricUnit}>{MetricUnit}</Text>
+        </View>
+        {/* Start Button */}
+        <View style={styles.bottomContainer}>
+          <Avatar
+            size={120}
+            rounded
+            title="START"
+            onPress={() => console.warn('Works!')}
+            activeOpacity={0.7}
+            titleStyle={styles.avatarTitle}
+            containerStyle={styles.avatarContainer}
+          />
+          {/* Toggle button to change the metric from distance or time */}
+          <Pressable onPress={toggleHandler} style={styles.toggleContainer}>
+            <Text style={styles.toggleTitle}>{Toggle}</Text>
+          </Pressable>
+        </View>
       </View>
     </View>
   );
